bb8b623fdc755e3019b5fee93c660d21686ef31d
Ifeanyi <58168058+ifeanyiokala@users.noreply.github.com>
2022-10-16 18:59:45 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import joblib
+import sklearn
+
+
+model = joblib.load('Weight.lr')
+labelize = joblib.load('labelized.le')
+
+# Title for the app
+
+st.title('Your Weight Predictor')
+sex = st.radio(label = 'Gender', options=("Male", "Female"))
+height = st.text_input(label='Enter Height', placeholder=" e.g (5.3)")
+
+
+if not st.button('Predict'):
+    pass
+else:
+
+    # 1. Labelized
+    sex = labelize.transform([sex])
+
+    # 2. Scale and Predict
+    result = model.predict([[sex, height]])[0]
+
+    # 3. Display
+    st.subheader(f"Predicted Weight is {round(result,2)} kg")
